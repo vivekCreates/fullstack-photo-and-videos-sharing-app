fbafb8d2d5a24ec10059fa84bb05926567e95576
vivek <codecrafter67@gmail.com>
2026-01-27 12:49:44 +0530

    update the usercreate schema

--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -1,10 +1,10 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr,Field
 from typing import Optional
 
 class UserCreate(BaseModel):
-    name: str
+    name: str = Field(...,min_length=3, max_length=50)
     email: EmailStr
-    password:str
+    password:str = Field(...,min_length=6, max_length=100)
     profile_image:Optional[str] = None
 
     class Config:
