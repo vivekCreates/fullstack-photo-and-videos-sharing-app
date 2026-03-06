97fdd57f9fe632982b4648a512747b7514c8d27d
vivek <codecrafter67@gmail.com>
2026-03-06 23:22:26 +0530

    Implement the bookmark post route and controller

---
+++ backend/app/routes/bookmark_route.py
@@ -0,0 +1,46 @@
+from fastapi import APIRouter,Depends,HTTPException
+from app.deps.auth_dep import get_current_user
+from sqlalchemy.orm import Session
+from app.db.session import get_db
+from app.models.bookmark_model import Bookmark
+from app.models.post_model import Post
+from app.schemas.response_schema import ApiResponse
+
+router = APIRouter(prefix="bookmarks")
+
+
+@router.post("/{post_id}")
+def bookmark_post(post_id:int,user=Depends(get_current_user),db:Session=Depends(get_db)):
+    try:
+        existed_post = db.query(Post).filter(Post.id == post_id).first()
+        if not existed_post:
+            return ApiResponse(
+                statusCode=404,
+                message="Post not found",
+            )
+            
+        bookmark = Bookmark(
+            user_id=user.id,
+            post_id=post_id
+        )
+        
+        db.add(bookmark)
+        db.commit()
+        db.refresh(bookmark)
+        
+        if not bookmark:
+            return ApiResponse(
+                statusCode=400,
+                message="Failed to bookmark post"
+            )
+        return ApiResponse(
+                statusCode=200,
+                message="Post bookmark successfully",
+                data=bookmark
+            ).model_dump()
+    except HTTPException as e:
+        print(str(e))
+        return ApiResponse(
+            statusCode=500,
+            message=str(e)
+        )
