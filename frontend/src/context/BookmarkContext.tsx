0b8e28fbcc602e841e202c2a56c4fbfc0de36bd0
vivek <codecrafter67@gmail.com>
2026-03-07 16:06:39 +0530

    refactor BookmarkContext: improve error handling and formatting for addToBookmark and removeToBookmark functions

--- frontend/src/context/BookmarkContext.tsx
+++ frontend/src/context/BookmarkContext.tsx
@@ -4,53 +4,81 @@ import { useAuth } from "./UserContext";
 import toast from "react-hot-toast";
 
 type BookmarkContextType = {
-    bookmarks:PostType[],
-    addToBookmark:(postId:number)=>void,
-    removeToBookmark:(postId:number)=>void,
-    getAllBookmarks:()=>void
+    bookmarks: PostType[],
+    addToBookmark: (postId: number) => void,
+    removeToBookmark: (postId: number) => void,
+    getAllBookmarks: () => void
 }
 
 const BookmarkContext = createContext<BookmarkContextType>({
-bookmarks:[],
-addToBookmark:async()=>{},
-removeToBookmark:async()=>{},
-getAllBookmarks:async()=>{}
-}) 
+    bookmarks: [],
+    addToBookmark: async () => { },
+    removeToBookmark: async () => { },
+    getAllBookmarks: async () => { }
+})
 
 const URL = "http://localhost:8000/api/bookmarks"
 
-const BookmarkContextProvider = ({children}:{children:React.ReactNode})=>{
-    const [bookmarks,setBookmarks] = useState<BookmarkContextType[]>()
-    const { token} = useAuth();
+const BookmarkContextProvider = ({ children }: { children: React.ReactNode }) => {
+    const [bookmarks, setBookmarks] = useState<BookmarkContextType[]>()
+    const { token } = useAuth();
 
 
-    const addToBookmark = async(id:number)=>{
+    const addToBookmark = async (id: number) => {
         try {
-            const response = await fetch(`${URL}/${id}`,{
-                method:"GET",
-                headers:{
-                    "Authorization":`Bearer ${token}`
+            const response = await fetch(`${URL}/${id}`, {
+                method: "GET",
+                headers: {
+                    "Authorization": `Bearer ${token}`
                 }
             })
 
-            if(!response.ok){
+            if (!response.ok) {
                 throw new Error("Failed to bookmark post")
             }
 
             const data = await response.json()
 
-            if(!data.success){
+            if (!data.success) {
                 throw new Error(data?.message || "Something went wrong")
             }
 
             toast.success(data.success)
-        } catch (error:any) {
+        } catch (error: any) {
             console.log(error?.message)
             toast.error(error?.message)
         }
     }
+
+    const removeToBookmark = async (id: number) => {
+        try {
+            const response = await fetch(`${URL}/${id}`, {
+                method: "DELETE",
+                headers: {
+                    "Authorization": `Bearer ${token}`
+                }
+            })
+
+            if (!response.ok) {
+                throw new Error("Failed to delete bookmark post")
+            }
+
+            const data = await response.json()
+
+            if (!data.success) {
+                throw new Error(data?.message || "Something went wrong")
+            }
+
+            toast.success(data.success)
+        } catch (error: any) {
+            console.log(error?.message)
+            toast.error(error?.message)
+        }
+    }
+
+    
     return (
-        <BookmarkContext.Provider value={{bookmarks,addToBookmark}}>
+        <BookmarkContext.Provider value={{ bookmarks, addToBookmark ,removeToBookmark,getAllBookmarks}}>
             {children}
         </BookmarkContext.Provider>
     )
