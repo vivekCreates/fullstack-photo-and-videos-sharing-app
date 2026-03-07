233fc035b79ffe997595b3a88a5bf1c650bfe1ec
vivek <codecrafter67@gmail.com>
2026-03-07 13:55:56 +0530

    implement the addtobookmark function in bookmarkContext

---
+++ frontend/src/context/BookmarkContext.tsx
@@ -0,0 +1,57 @@
+import { createContext, useState } from "react";
+import type { PostType } from "../types/post";
+import { useAuth } from "./UserContext";
+import toast from "react-hot-toast";
+
+type BookmarkContextType = {
+    bookmarks:PostType[],
+    addToBookmark:(postId:number)=>void,
+    removeToBookmark:(postId:number)=>void,
+    getAllBookmarks:()=>void
+}
+
+const BookmarkContext = createContext<BookmarkContextType>({
+bookmarks:[],
+addToBookmark:async()=>{},
+removeToBookmark:async()=>{},
+getAllBookmarks:async()=>{}
+}) 
+
+const URL = "http://localhost:8000/api/bookmarks"
+
+const BookmarkContextProvider = ({children}:{children:React.ReactNode})=>{
+    const [bookmarks,setBookmarks] = useState<BookmarkContextType[]>()
+    const { token} = useAuth();
+
+
+    const addToBookmark = async(id:number)=>{
+        try {
+            const response = await fetch(`${URL}/${id}`,{
+                method:"GET",
+                headers:{
+                    "Authorization":`Bearer ${token}`
+                }
+            })
+
+            if(!response.ok){
+                throw new Error("Failed to bookmark post")
+            }
+
+            const data = await response.json()
+
+            if(!data.success){
+                throw new Error(data?.message || "Something went wrong")
+            }
+
+            toast.success(data.success)
+        } catch (error:any) {
+            console.log(error?.message)
+            toast.error(error?.message)
+        }
+    }
+    return (
+        <BookmarkContext.Provider value={{bookmarks,addToBookmark}}>
+            {children}
+        </BookmarkContext.Provider>
+    )
+}
