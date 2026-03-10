c5fe1b2e048352b2350e398242883d1093708fd9
vivek <codecrafter67@gmail.com>
2026-03-10 23:59:22 +0530

    implement the bookmark api and apply on BookmarkContext

--- frontend/src/context/BookmarkContext.tsx
+++ frontend/src/context/BookmarkContext.tsx
@@ -3,15 +3,21 @@ import { useAuth } from "./UserContext";
 import toast from "react-hot-toast";
 import type { BookmarkPost } from "../types/bookmark";
 import { usePost } from "./PostContext";
+import { requestHandler } from "../utils/requestHandler";
+import { getAllBookmarksApi, toggleBookmarkApi } from "../api/bookmark";
 
 type BookmarkContextType = {
     bookmarks: BookmarkPost[],
     toggleBookmark: (postId: number) => void,
+    setCreateLoading:(loading:boolean)=>void,
+    createLoading:boolean,
     getAllBookmarks: () => void
 }
 
 const BookmarkContext = createContext<BookmarkContextType>({
     bookmarks: [],
+    setCreateLoading:()=>{},
+    createLoading:false,
     toggleBookmark: async () => { },
     getAllBookmarks: async () => { }
 })
@@ -22,66 +28,40 @@ export const BookmarkContextProvider = ({ children }: { children: React.ReactNod
     const [bookmarks, setBookmarks] = useState<BookmarkPost[]>([])
     const {token } = useAuth();
     const {setPosts} = usePost();
-
+    const [createLoading,setCreateLoading] = useState(false);
  
     const toggleBookmark = async (id: number) => {
         setPosts(prev=>prev.map(b=>b.id == id ? {...b,isBookmark:!b.isBookmark}:b))
-        try {
-            const response = await fetch(`${URL}/${id}`, {
-                method: "POST",
-                headers: {
-                    "Authorization": `Bearer ${token}`
-                }
-            })
-
-            if (!response.ok) {
-                throw new Error("Failed to bookmark post")
+        await requestHandler(
+            async()=>await toggleBookmarkApi(id),
+            setCreateLoading,
+            (res)=>{
+                toast.success(res.message)
+            },
+            (error)=>{
+                setPosts(prev=>prev.map(b=>b.id == id ? {...b,isBookmark:!b.isBookmark}:b))
+                toast.error(error)
             }
-
-            const data = await response.json()
-
-            if (!data.success) {
-                throw new Error(data?.message || "Something went wrong")
-            }
-
-            toast.success(data.message)
-        } catch (error: any) {
-             setPosts(prev=>prev.map(b=>b.id == id ? {...b,isBookmark:!b.isBookmark}:b))
-            console.log(error?.message)
-            toast.error(error?.message)
-        }
+        )
     }
 
     const getAllBookmarks = async()=>{
-          try {
-            const response = await fetch(`${URL}`,{
-                method:"GET",
-                headers:{
-                    "Authorization":`Bearer ${token}`
-                }
-            })
-
-            if(!response.ok){
-                throw new Error("Failed to bookmark post")
+         await requestHandler(
+            async()=>await getAllBookmarksApi(),
+            setCreateLoading,
+            (res)=>{
+                const data = res.data;
+                setBookmarks(data);
+                toast.success(res.message)
+            },
+            (error)=>{
+                toast.error(error)
             }
-
-            const data = await response.json()
-
-            if(!data.success){
-                throw new Error(data?.message || "Something went wrong")
-            }
-            console.log("data: ",data)
-            setBookmarks(data?.data)
-            toast.success(data.message)
-            return bookmarks
-        } catch (error:any) {
-            console.log(error?.message)
-            toast.error(error?.message)
-        }
+        )
     }
 
     return (
-        <BookmarkContext.Provider value={{ bookmarks, toggleBookmark,getAllBookmarks}}>
+        <BookmarkContext.Provider value={{ bookmarks, toggleBookmark,getAllBookmarks,createLoading,setCreateLoading}}>
             {children}
         </BookmarkContext.Provider>
     )
